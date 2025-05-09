c500f1413418acd8a61093758cedb1f2e7929735
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-05-08 21:35:19 -0300

    Exercicío 4

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+
+double salario_base, salario_horas, aumento, valor_extra, salario_extras;
+int horas_trabalhadas, horas_extras;
+
+Console.Write("Digite seu salário em horas: ");
+salario_base = double.Parse(Console.ReadLine());
+
+Console.Write("Digite o total de horas trabalhadas: ");
+horas_trabalhadas = int.Parse(Console.ReadLine());
+
+Console.Write("Digite as horas-extras executadas: ");
+horas_extras = int.Parse(Console.ReadLine());
+
+salario_horas = salario_base * horas_trabalhadas;
+
+valor_extra= salario_base * 1.40;
+
+salario_extras = valor_extra * horas_extras;
+
+aumento = salario_horas + salario_extras;
+
+Console.Write("O seu salário depois das horas-extras é de: {0:c2}", aumento);
